b630d944070800a0eaa8ac1e75591e718defa76e
OkekeChristian24 <christian.okeke24@gmail.com>
2022-03-24 03:01:15 -0700

    fix: include database settings in config

---
+++ config/database.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+dotenv.config({path: './config/config.env'});
+
+const host = process.env.HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+
+const dbOptions = {
+    connectionLimit : 10,
+    host            : host,
+    user            : user,
+    password        : password,
+    database        : database
+};
+
+const sessionOptions = {
+    clearExpired: true,
+	checkExpirationInterval: 900000,
+	expiration: 86400000,
+	createDatabaseTable: true,
+	endConnectionOnClose: true,
+	charset: 'utf8mb4_bin',
+	schema: {
+		tableName: 'sessions',
+		columnNames: {
+			session_id: 'session_id',
+			expires: 'expires',
+			data: 'data'
+		}
+	}
+};
+
+const pool  = mysql.createPool(dbOptions);
+const sessionStore = new MySQLStore(sessionOptions, pool);
+
+module.exports = {
+    pool,
+    sessionStore
+};
