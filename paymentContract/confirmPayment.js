d3f871d4946ea171842339377b788ddd0d9ec560
OkekeChristian24 <christian.okeke24@gmail.com>
2022-03-25 02:54:31 -0700

    fix: add db port number to connection setting

--- paymentContract/confirmPayment.js
+++ paymentContract/confirmPayment.js
@@ -5,38 +5,48 @@ const checkTotalPrice = require("../utils/checkTotalPrice");
 
 
 const confirmPaymentOnFantom = async(buyer, orderID, paymentID, totalPrice, chainID, tokenIndex, products, company) => {
-    const paymentDetails = await FTMcontract.methods.getTransactionDetails(buyer, orderID).call();
-    
-    const txnPrice = convertedAmt(paymentDetails.totalPrice, fantomHexToNumString(chainID), tokenIndex)
-    const isValidPrice = await checkTotalPrice(txnPrice, products, company);
-    if(!isValidPrice){
-        return false;
-    };
-    
-    if(
-        paymentID !== String(paymentDetails.paymentID)
-    ){
+    try {
+        const paymentDetails = await FTMcontract.methods.getTransactionDetails(buyer, orderID).call();
+        const txnPrice = convertedAmt(paymentDetails.totalPrice, fantomHexToNumString(chainID), tokenIndex)
+        const isValidPrice = await checkTotalPrice(txnPrice, products, company);
+        if(!isValidPrice){
+            return false;
+        };
+        
+        if(
+            paymentID !== String(paymentDetails.paymentID)
+        ){
+            return false;
+        }
+        return true;
+        
+    } catch (error) {
+        console.log(error);
         return false;
     }
-    return true;
 };
 
 
-// getTransactionDetails(address buyer, string memory orderId)
 const confirmPaymentOnBSC = async(buyer, orderID, paymentID, totalPrice, chainID, tokenIndex, products, company) => {
-    const paymentDetails = await BSCcontract.methods.getTransactionDetails(buyer, orderID).call();
-    const txnPrice = convertedAmt(paymentDetails.totalPrice, bscHexToNumString(chainID), tokenIndex)
-    const isValidPrice = await checkTotalPrice(txnPrice, products, company);    
-    if(!isValidPrice){
-        return false
-    };
-    
-    if(
-        paymentID !== String(paymentDetails.paymentID) 
-    ){
+    try {
+        const paymentDetails = await BSCcontract.methods.getTransactionDetails(buyer, orderID).call();
+        const txnPrice = convertedAmt(paymentDetails.totalPrice, bscHexToNumString(chainID), tokenIndex)
+        const isValidPrice = await checkTotalPrice(txnPrice, products, company);    
+        if(!isValidPrice){
+            return false
+        };
+        
+        if(
+            paymentID !== String(paymentDetails.paymentID) 
+        ){
+            return false;
+        }
+        return true;
+        
+    } catch (error) {
+        console.log(error);
         return false;
     }
-    return true;
 };
 
 module.exports = {
